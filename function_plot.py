66ec5599526b1313a7251106a5fc87bb8df4e08f
AgusSolari <agustinraigoso@hotmail.com>
2023-06-15 12:38:49 -0300

    plots

--- function_plot.py
+++ function_plot.py
@@ -1,48 +1,73 @@
 import input_signals as isig
 import matplotlib.pyplot as plt
+import numpy as np
 
 input_signal = isig.input_signal()
 
-# def update_input_plot(self):
-#     self.MplWidget.canvas.axes.clear()
-#     self.MplWidget.canvas.axes.plot(input_signal.tt, input_signal.st)
-#     self.MplWidget.canvas.axes.legend(('test1', 'test2'))
-#     self.MplWidget.canvas.draw()
 
 def generate_input_signal(self):
     print("Generating input signal")
+
     if self.feSenoide_button.isChecked():
+        a0 = self.feAmplitud_text.text()
+        f0 = self.feFrecuencia_text.text()
+
+        if a0 != '' and f0 != '' and float(a0) > 0 and float(f0) > 0:
+            # self.update_input_plot(self)
+            input_signal = isig.generate_sinusoidal_signal(100, float(f0), 100, float(a0), 0)
+            #plot input signal with matplotlib
+            print("Plotting input signal")
+            tt, st = input_signal
+
+            self.MplWidget.canvas.axes.clear()
+            self.MplWidget.canvas.axes.plot(tt, st)
+            self.MplWidget.canvas.axes.legend(('test1', 'test2'))
+            self.MplWidget.canvas.draw()
+
+        else:
+            self.MplWidget.canvas.axes.clear()
+            self.MplWidget.canvas.draw()
+
+
+
+    elif self.feEscalon_button.isChecked():
+        a0 = self.feAmplitud_text.text()
+
+        if a0 != '' and float(a0) > 0:
+            # self.update_input_plot(self)
+            input_signal = isig.generate_step_signal(100, 100, float(a0))
+            #plot input signal with matplotlib
+            print("Plotting input signal")
+            tt, st = input_signal
+
+
+            self.MplWidget.canvas.axes.clear()
+            self.MplWidget.canvas.axes.plot(tt, st)
+            self.MplWidget.canvas.axes.legend(('test1', 'test2'))
+            self.MplWidget.canvas.draw()
+        else:
+            self.MplWidget.canvas.axes.clear()
+            self.MplWidget.canvas.draw()
+
+
+    elif self.fePulso_button.isChecked():
 
         a0 = self.feAmplitud_text.text()
         f0 = self.feFrecuencia_text.text()
 
         if a0 != '' and f0 != '' and float(a0) > 0 and float(f0) > 0:
             # self.update_input_plot(self)
-            input_signal = isig.generate_sinusoidal_signal(100, float(f0), 1, float(a0), 0)
+            input_signal = isig.generate_square_signal(100, float(f0), 100, float(a0), 0, 0.5)
             #plot input signal with matplotlib
-            plt.plot(input_signal)
-            plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
+            print("Plotting input signal")
+            tt, st = input_signal
 
             self.MplWidget.canvas.axes.clear()
-            self.MplWidget.canvas.axes.plot(isig.generate_sinusoidal_signal(100, float(f0), 1, float(a0), 0))
+            self.MplWidget.canvas.axes.plot(tt, st)
             self.MplWidget.canvas.axes.legend(('test1', 'test2'))
             self.MplWidget.canvas.draw()
 
-
-    elif self.feEscalon_button.isChecked():
-            print("Escalon")
-    elif self.fePulso_button.isChecked():
-            print("Pulso")
+        else:
+            self.MplWidget.canvas.axes.clear()
+            self.MplWidget.canvas.draw()
 
